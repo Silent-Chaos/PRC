1050897b4d0a84b66a93cfb6722d73ff38210834
SilentChaos <avanishm471@gmail.com>
2023-02-13 18:00:50 +0530

    first commit

---
+++ if else/index1.js
@@ -0,0 +1,23 @@
+let age=15
+
+if(age<6)
+{
+    console.log("free")
+
+}
+if(age >=6&&age<=17)
+{
+    console.log("child discount")
+}
+if(age >=18&&age<=26)
+{
+    console.log("student discount")
+}
+if(age >=27&&age<=66)
+{
+    console.log("full price")
+}
+if(age >66)
+{
+    console.log("senior citizen discount")
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+let person={
+    name:"avanish",
+    age:19,
+    country:"india",
+
+
+}
+function logData(){
+    console.log(person.name+" is"+person.age+"years old and lives in "+person.country)
+}
+logData()
